ea4f988f5a237485061d3b08d9cb23465b943f39
leanhduc04 <ducla2204@gmail.com>
2022-07-02 10:24:43 +0700

    Function Homework

--- function.js
+++ function.js
@@ -1,11 +1,10 @@
 //Bài 1:
 //Đầu vào: "Hello"
 var str = "Hello"
-function a(str){
- var uplow = str.charAt(0).toLowerCase() + str.slice(1).toUpperCase()
- return uplow
+function reverseString(str){
+return str.split("").map(i => i === i.toLowerCase() ? i.toUpperCase() : i.toLowerCase() ).join("")
 }
-console.log(a(str))
+console.log(reverseString(str))
 //Trả về: "hELLO"
 
 
@@ -32,21 +31,22 @@ console.log(random(arr2))
 //Đầu vào: 6
 //Trả về: “Sáng"
 function time(hour){
-     0 < hour && hour <= 24
-    if(0 < hour && hour <10){
-        return ("Sáng")
+  if (0 < hour && hour <= 24){
+    if(hour <10){
+        return "Sáng"
     }
     else if (10 <= hour && hour <= 12) {
-        return ("Trưa")
+        return "Trưa"
     }
     else if (12 < hour && hour <18) {
-        return ("Chiều")
+        return "Chiều"
     }
     else {
-        return ("Tối")
+        return "Tối"
     }
 }
-console.log(time(10))
+}
+console.log(time(22))
 
 
 
@@ -54,31 +54,15 @@ console.log(time(10))
 //Bài 4:
 //Đầu vào: [1, 3, 2, 8, 4, 5]
 var arr4=[1, 3, 2, 8, 4, 5]
-function max(arr4){
-     let max = arr4[0]
-     for (let i = 1; i < arr4.length; i++)
-     if (max < arr4[i])
-         max = arr4[i];
-         return max
-}
-console.log(max(arr4)) 
+function maxMinSum(arr4){
+var max = arr4.sort().reverse()[0]
+var min = arr4.sort()[0]
+var sum = arr4.reduce((reduce,element) => reduce + element)
 
-function min(arr4){
-    let min = arr4[0]
-    for (let i = 1; i < arr4.length; i++)
-    if (min > arr4[i])
-        min = arr4[i];
-        return min
+return {max, min, sum}
 }
-console.log(min(arr4))
 
-function sum(arr4){
-    var tong=0
-    for (let i = 0; i < arr4.length; i++)
-    tong+=arr4[i]
-    return tong
-}
-console.log(sum(arr4))
+console.log(maxMinSum(arr4))
 //Trả về: { max: 8, min: 1, sum: 23 }
 
 
@@ -99,8 +83,7 @@ console.log(sum(arr4))
 //Đầu vào: 3
 //Trả về: true
 function DaoNguoc(value){
-    var boo = (value === +value.toString().split("").reverse().join("")) 
-    return boo
+    return (value === +value.toString().split("").reverse().join("")) 
 }
 console.log(DaoNguoc(123321))
 
@@ -121,8 +104,11 @@ console.log(DaoNguoc(123321))
 //--------------------
 //Đầu vào: ngang: 0, cao: 3
 //Trả về: Width or height can not equal 0
-
-
+function matrix(width,height){
+    if (width === 0 || height === 0) return "Width or height can not equal 0"
+    return `${"0 ".repeat(width)}\n`.repeat(height)
+}
+console.log(matrix(2,3))
 
 //Bài 7:
 //Đầu vào: [1, …, 32, 33, 34, 35, 35, 36, …, 99] (dấu ba chấm viết tắt cho các số tăng dần lần lượt)
@@ -146,8 +132,7 @@ console.log(laplai(arr7))
 //Đầu vào: [1,[2,3,null,4],[null],5]
 var arr8 = [1,[2,3,null,4],[null],5]
 function flat(arr8){
-    kq = arr8.flat().filter(arr8 => arr8 != null)
-    return kq 
+    return arr8.flat().filter(arr8 => arr8 != null) 
 }
 console.log(flat(arr8))
 //Trả về: [1,2,3,4,5]
@@ -163,3 +148,15 @@ console.log(flat(arr8))
 //--------------------
 //Đầu vào: chuỗi: “5242”, số: 6
 //Trả về: [“5242”]
+function abc(str, num) {
+    if (str.length < num) return [str]
+
+    var arr =[]
+    var count = str.length - num + 1
+    for (let i = 0;i < count;i++) {
+        var ele = str.split("").splice(i, num).join("")
+        arr.push(ele)
+    }
+    return arr
+}
+console.log(abc("123456",3))
